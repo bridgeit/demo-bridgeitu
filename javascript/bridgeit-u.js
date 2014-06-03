c2b05dcb6e580724169090203c2adc76506045b4
bkroeger <bkroeger@shaw.ca>
2014-06-03 17:14:45 -0600

    Initial check-in of event notification panel.  The associated flow is not fully implemented.

--- javascript/bridgeit-u.js
+++ javascript/bridgeit-u.js
@@ -2,6 +2,7 @@ window.documentService = 'http://dev.bridgeit.io/docs/bridgeit.u/documents';
 window.authService = 'http://dev.bridgeit.io/auth/bridgeit.u/token/local';
 window.authServicePermissions = 'http://dev.bridgeit.io/auth/bridgeit.u/token/permissions';
 window.purchaseFlow = 'http://dev.bridgeit.io/code/bridgeit.u/purchase';
+window.eventNotificationFlow = 'http://dev.bridgeit.io/code/bridgeit.u/eventnotification';
 // Token obtained automatically to view events in the index.html screen without a login
 window.tokenAnonymousAccess = null;
 // Token obtained from a login
@@ -73,7 +74,7 @@ function studentLoginDone(data, textStatus, jqxhr){
         // Login is required to retrieve a token so purchases can be made
         window.tokenLoggedIn = data.access_token;
         uiLoggedIn();
-        $('#ticketsPanel').show();
+        $('#ticketsPanel').show('slow');
     }else{
         serviceRequestUnexpectedStatusAlert('Login', jqxhr.status);
     }
@@ -183,7 +184,7 @@ function adminRetrieveEventsDone(data, textStatus, jqxhr){
             if(!obj.access_token){
                 // Store the name Strings in the page to avoid encoding/decoding Strings coming from the service that may be used in javascript methods
                 window.events[obj._id] = obj.name;
-                evntLstDiv.append('<div class="list-group-item">' + obj.name + '<a title="Delete Event" onclick="deleteEvent(\'' + obj._id + '\');" class="pull-right"><span style="padding: 0 10px;"  class="glyphicon glyphicon-remove-circle"></span></a><a title="Edit Event" data-toggle="modal" href="#editModal" onclick="editEvent(\'' + obj._id + '\');" class="pull-right"><span class="glyphicon glyphicon-edit"></span></a></div>');
+                evntLstDiv.append('<div class="list-group-item"><a title="Event Notification" data-toggle="modal" href="#evntNtfctnModal" onclick="notifyEvent(\'' + obj.name + '\');"><span style="margin-right: 10px;" class="glyphicon glyphicon-bullhorn"></span></a>' + obj.name + '<a title="Delete Event" onclick="deleteEvent(\'' + obj._id + '\');" class="pull-right"><span style="margin-left: 10px;" class="glyphicon glyphicon-remove-circle"></span></a><a title="Edit Event" data-toggle="modal" href="#editModal" onclick="editEvent(\'' + obj._id + '\');" class="pull-right"><span class="glyphicon glyphicon-edit"></span></a></div>');
             }
         });
     }else{
@@ -339,6 +340,56 @@ var editEventDone = function(documentId){
     };
 };
 
+function notifyEvent(eventName){
+    $('#ntfctnTextLabel').html(eventName);
+    notifyEventShow();
+    $('#evntNtfctnFrm').off('submit').on('submit',(function( event ) {
+        event.preventDefault();
+        /* form element used to generically validate form elements (could also serialize the form if necessary)
+        *  Also using form to create json post data from form's elements
+        */
+        var form = this;
+        if(validate(form)){
+            var postData = {};
+            postData['access_token'] = window.tokenLoggedIn;
+            postData['name'] = form[0].value;
+            $.ajax({
+                url : window.eventNotificationFlow,
+                type: 'POST',
+                dataType : 'json',
+                contentType: 'application/json; charset=utf-8',
+                data : JSON.stringify(postData)
+            })
+            .fail(notifyEventFail)
+            .done(notifyEventDone);
+        }
+    }));
+}
+
+function notifyEventFail(jqxhr, textStatus, errorThrown){
+    if(jqxhr.status == 401){
+        // 401 unauthorized
+        $('#alertDiv').prepend(
+            $('<div class="alert alert-danger fade in"><button type="button" class="close" data-dismiss="alert" onclick="removeNoticesInfoClass();" aria-hidden="true">&times;</button><small><strong>Unauthorized</strong> to send notifications: status <strong>' + jqxhr.status + '</strong></small></div>').hide().fadeIn('slow')
+        );
+        $('#noticesPanel').addClass('panel-info');
+    }else{
+        requestFail(jqxhr, textStatus, errorThrown);
+    }
+}
+
+function notifyEventDone(data, textStatus, jqxhr){
+    if(jqxhr.status == 200){
+        $('#alertDiv').prepend(
+            $('<div class="alert alert-success fade in"><button type="button" class="close" data-dismiss="alert" onclick="removeNoticesInfoClass();" aria-hidden="true">&times;</button><small><strong>' + data.name + '</strong> notified.</small></div>').hide().fadeIn('slow')
+        );
+        $('#noticesPanel').addClass('panel-info');
+        toggleCreateNotifyEvent();
+    }else{
+        serviceRequestUnexpectedStatusAlert('Purchase', jqxhr.status);
+    }
+}
+
 function createEventSubmit(){
     $('#crtEvntFrm').submit(function( event ) {
         event.preventDefault();
@@ -423,10 +474,25 @@ function resetLoginForm(){
 }
 
 function resetFormCSS(form){
-    $(form[0]).parent('div').removeClass('has-error');
-    $(form[1]).parent('div').removeClass('has-error');
+    for(var i=0; i<form.length; i++){
+        if(form[i].tagName == 'INPUT' || form[i].tagName == 'TEXTAREA'){
+            $(form[i]).parent('div').removeClass('has-error');
+        }
+    }
 }
 
 function removeNoticesInfoClass(){
     $('#noticesPanel').removeClass('panel-info');
+}
+
+function notifyEventShow(){
+    var notificationForm = document.getElementById('evntNtfctnFrm');
+    notificationForm.reset();
+    $('#crtEvntDiv').hide();
+    $('#evntNtfctnDiv').show('slow');
+}
+
+function toggleCreateNotifyEvent(){
+    $('#evntNtfctnDiv').hide();
+    $('#crtEvntDiv').show('slow');
 }
